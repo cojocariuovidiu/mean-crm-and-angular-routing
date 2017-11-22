85fb78b561208f337fba3f90cadc84882181f764
1185303 <divya.singh7@tcs.com>
2017-11-22 20:18:05 +0530

    Routing and CRM

---
+++ Angular_routing/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+  var app     = express();
+  var path = require('path');
+  
+  // set the public folder to serve public assets
+  app.use(express.static(__dirname + '/'));
+  
+  // set up our one route to the index.html file
+ app.get('*', function(req, res) {
+     res.sendFile(path.join(__dirname + '/Angular-route-index.html'));
+ });
+ app.listen(8080);
+ console.log('port is running');
---
+++ mean-app-user-CRM-master/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	'port': process.env.PORT || 8050,
+	//'database': 'mongodb://killer:drama@ds115166.mlab.com:15166/usercrm',
+		'database': 'mongodb://localhost:27017/testdb',
+	'secret': 'ilovescotchscotchyscotchscotch'
+};
